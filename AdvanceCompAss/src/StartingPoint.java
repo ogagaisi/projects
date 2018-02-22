9313aab0e5072167265696f30aa444a6f22c2116
Ogaga.I <peterisiavwe@gmail.com>
2018-02-22 19:11:40 +0000

    Removing empty values
    The code now reads from the passenger file and the airport file.
    
    Added a functionality that removes leading and trailing white spaces
    from an element in the list , and removes empty elements from the list
    (The document is converted into a list in which each element represents
    a line)

--- AdvanceCompAss/src/StartingPoint.java
+++ AdvanceCompAss/src/StartingPoint.java
@@ -6,11 +6,18 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class StartingPoint {
 	
-	public static List<String>  readFile(String path, Charset encoding){// Reads data from a file and puts it in a string list
+	/**
+	 * Reads data from a file and puts it in a string list
+	 * @param path the path of the file to be read
+	 * @param encoding the character set the file should be encoded in
+	 * @return lines a String<list> representing each line in the document.
+	 * */
+	public static List<String>  readFile(String path, Charset encoding){
 		List<String> lines = new ArrayList<String>();
 		
 		try{
@@ -24,17 +31,39 @@ public class StartingPoint {
 			ex.printStackTrace();
 		}
 		System.out.println("The file at: '" + path + "' has been read");
+		
+		// Removes blank values from the list. 
+	    Iterator<String> i = lines.iterator();
+	    while (i.hasNext())
+	    {
+	        String line = i.next();
+	        if (line.trim().isEmpty()) // Removes leading and trailing white spaces, and checks if that element is empty
+	            i.remove(); // remove the element 
+	    }
+	    
 		return lines;
 	}
 	
 	public static void main(String[] args) {
 		
-		String path = "C:\\Users\\ogaga isiavwe\\Desktop\\csv files\\AComp_Passenger_data_no_error.csv";
+		String passengerDataPath = "C:\\Users\\ogaga isiavwe\\Desktop\\csv files\\AComp_Passenger_data_no_error.csv";
+		String airportDataPath = "C:\\Users\\ogaga isiavwe\\Desktop\\csv files\\Top30_airports_LatLong.csv";
+		
 		List<String> flightData = new ArrayList<String>();// Stores the data from "AComp_Passenger_data.csv"
+		List<String> airportData = new ArrayList<String>(); // Stores the data from "Top30_airports_LatLong"
 		
-		flightData = readFile(path, StandardCharsets.UTF_8);
+		flightData = readFile(passengerDataPath, StandardCharsets.UTF_8);
+		airportData = readFile(airportDataPath, StandardCharsets.UTF_8);
 		
+		System.out.println("Passenegr Data:");
 		for(String data: flightData){
+			System.out.println(data);		
+		}
+		System.out.println("");
+		System.out.println("");
+		
+		System.out.println("Airport Data:");
+		for(String data: airportData){
 			System.out.println(data);		
 		}
 	}
