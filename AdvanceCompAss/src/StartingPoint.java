4197e4fa243964f632e58a498fd85b608064fbb3
Ogaga.I <peterisiavwe@gmail.com>
2018-02-24 17:24:34 +0000

    Updated readFile
    It now returns a list of list<String> representing each line in the
    document, and the individual comma separated values.
    Memory management was added to gradually delete the variable containing
    the whole document as it is pushed in the list of lists.

--- AdvanceCompAss/src/StartingPoint.java
+++ AdvanceCompAss/src/StartingPoint.java
@@ -6,6 +6,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
@@ -15,10 +16,12 @@ public class StartingPoint {
 	 * Reads data from a file and puts it in a string list
 	 * @param path the path of the file to be read
 	 * @param encoding the character set the file should be encoded in
-	 * @return lines a String<list> representing each line in the document.
+	 * @param lines a String<list> representing each line in the document.
+	 * @param structuredData a List<List<String>> which stores all the elements in each line
 	 * */
-	public static List<String>  readFile(String path, Charset encoding){
+	public static List<List<String>>  readFile(String path, Charset encoding){
 		List<String> lines = new ArrayList<String>();
+		List<List<String>> structuredData = new ArrayList<>();
 		
 		try{
 			 lines= Files.readAllLines(Paths.get(path), encoding);
@@ -37,11 +40,17 @@ public class StartingPoint {
 	    while (i.hasNext())
 	    {
 	        String line = i.next();
-	        if (line.trim().isEmpty()) // Removes leading and trailing white spaces, and checks if that element is empty
-	            i.remove(); // remove the element 
+	        if (line.trim().isEmpty()) {// Removes leading and trailing white spaces, and checks if that element is empty
+	            i.remove(); // remove the element
+	        }
+	        else{
+	        	structuredData.add(new ArrayList<>(Arrays.asList(line.split(",")))); // Splits line into a string array e.g "1,2,3" --> ["1", "2", "3"], Arrays.asList() converts the array into an unmodifiable List, and new ArrayList<>() ensures the list is modifiable
+	        	i.remove(); // Memory management 
+	        }
+	        
 	    }
-	    
-		return lines;
+	     	    		
+		return structuredData;
 	}
 	
 	public static void main(String[] args) {
@@ -49,23 +58,17 @@ public class StartingPoint {
 		String passengerDataPath = "C:\\Users\\ogaga isiavwe\\Desktop\\csv files\\AComp_Passenger_data_no_error.csv";
 		String airportDataPath = "C:\\Users\\ogaga isiavwe\\Desktop\\csv files\\Top30_airports_LatLong.csv";
 		
-		List<String> flightData = new ArrayList<String>();// Stores the data from "AComp_Passenger_data.csv"
-		List<String> airportData = new ArrayList<String>(); // Stores the data from "Top30_airports_LatLong"
+		List<List<String>> flightData = new ArrayList<>();// Stores the data from "AComp_Passenger_data.csv"
+		List<List<String>> airportData = new ArrayList<>(); // Stores the data from "Top30_airports_LatLong"
+
 		
 		flightData = readFile(passengerDataPath, StandardCharsets.UTF_8);
 		airportData = readFile(airportDataPath, StandardCharsets.UTF_8);
 		
-		System.out.println("Passenegr Data:");
-		for(String data: flightData){
+		for(List<String> data: flightData){
 			System.out.println(data);		
 		}
-		System.out.println("");
-		System.out.println("");
 		
-		System.out.println("Airport Data:");
-		for(String data: airportData){
-			System.out.println(data);		
-		}
 	}
 
 }
